90ecfbf61bc1812cda8d3c254cb8911629a36b19
leonardopf26 <leonardopf26@gmail.com>
2021-04-11 13:17:13 -0300

    Added new transactions modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,13 +2,19 @@ import logoImg from "../../assets/logo.svg";
 
 import { Container, Content } from "./styles";
 
-export const Header: React.FC = () => {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="djmoney" />
-        <button>Nova Transação</button>
+        <button type="button" onClick={onOpenNewTransactionModal}>
+          Nova Transação
+        </button>
       </Content>
     </Container>
   );
-};
+}
