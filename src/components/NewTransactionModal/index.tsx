362f18e74d856807b728979b0f19f82eb5e128c3
leonardopf26 <leonardopf26@gmail.com>
2021-04-14 17:21:06 -0300

    Added refresh table when new transaction is create

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -3,7 +3,6 @@ import { FormEvent, useState, useContext } from "react";
 import Modal from "react-modal";
 
 import { TransactionsCotext } from "../../TransactionsContext";
-import { api } from "../../services/api";
 
 import closeImg from "../../assets/close.svg";
 import incomeImg from "../../assets/income.svg";
@@ -27,10 +26,16 @@ export function NewTransactionModal({
   const [category, setCategory] = useState("");
   const [type, setType] = useState("deposit");
 
-  function handleCreateNewTransaction(event: FormEvent) {
+  async function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    createTransaction({ amount, category, title, type });
+    await createTransaction({ amount, category, title, type });
+
+    setTitle("");
+    setAmount(0);
+    setCategory("");
+    setType("deposit");
+    onRequestClose();
   }
 
   return (
